f64d06fe9b09e4aa0b9678dce19fc1d012652e30
cnakpil <85041294+cnakpil@users.noreply.github.com>
2021-10-01 21:40:38 -0600

    temp wk 6

---
+++ week6/external_javascript.js
@@ -0,0 +1,41 @@
+document.getElementById("androids").addEventListener("click", androids);
+document.getElementById("humans").addEventListener("click", humans);
+document.getElementById("restart").addEventListener("click", restart);
+
+function restart(){
+  document.getElementById("line1").innerHTML = 'what do <a id="androids">androids</a>/<a id="humans">humans</a> dream?';
+  document.getElementById("line2").innerHTML = "";
+  document.getElementById("line3").innerHTML = "";
+  document.getElementById("androids").addEventListener("click", androids);
+  document.getElementById("humans").addEventListener("click", humans);
+  document.getElementById("restart").addEventListener("click", restart);
+}
+
+function androids(){
+  document.getElementById("line1").innerHTML = "what do androids dream?";
+  document.getElementById("line2").innerHTML = 'moments of life <a id="bytes">stored in bytes</a>/<a id="time">lost to time</a>';
+  document.getElementById("bytes").addEventListener("click", bytes);
+  document.getElementById("time").addEventListener("click", time);
+}
+function bytes(){
+  document.getElementById("line2").innerHTML = "moments of life stored in bytes";
+  document.getElementById("line3").innerHTML = "immortality";
+}
+function time(){
+  document.getElementById("line2").innerHTML = "moments of life lost to time";
+  document.getElementById("line3").innerHTML = "gone like tears in rain";
+}
+function humans(){
+  document.getElementById("line1").innerHTML = "what do humans dream?";
+  document.getElementById("line2").innerHTML = 'of <a id="flares">solar flares</a>/<a id="fires">wildfires</a> burning bright';
+  document.getElementById("flares").addEventListener("click", flares);
+  document.getElementById("fires").addEventListener("click", fires);
+}
+function flares(){
+  document.getElementById("line2").innerHTML = "of solar flares burning bright";
+  document.getElementById("line3").innerHTML = "tiny galaxies";
+}
+function fires(){
+  document.getElementById("line2").innerHTML = "of wildfires burning bright";
+  document.getElementById("line3").innerHTML = "scorched from dragon's breath";
+}
