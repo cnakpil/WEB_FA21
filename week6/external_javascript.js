74e92b80a207e6f365c296fb48e8f686f60d0d06
cnakpil <85041294+cnakpil@users.noreply.github.com>
2021-10-07 16:29:29 -0600

    week6 update

--- week6/external_javascript.js
+++ week6/external_javascript.js
@@ -6,10 +6,12 @@ function restart(){
   document.getElementById("line1").innerHTML = 'what do <a id="androids">androids</a>/<a id="humans">humans</a> dream?';
   document.getElementById("line2").innerHTML = "";
   document.getElementById("line3").innerHTML = "";
+  document.getElementById("restart").innerHTML = "";
   document.getElementById("androids").addEventListener("click", androids);
   document.getElementById("humans").addEventListener("click", humans);
   document.getElementById("restart").addEventListener("click", restart);
   document.body.style.backgroundImage = 'none';
+
 }
 function androids(){
   document.getElementById("line1").innerHTML = "what do androids dream?";
@@ -20,11 +22,13 @@ function androids(){
 function bytes(){
   document.getElementById("line2").innerHTML = "moments of life stored in bytes";
   document.getElementById("line3").innerHTML = "immortality";
+  document.getElementById("restart").innerHTML = "restart";
   document.body.style.backgroundImage = "url('./assets/AI.png')";
 }
 function time(){
   document.getElementById("line2").innerHTML = "moments of life lost to time";
   document.getElementById("line3").innerHTML = "gone like tears in rain";
+  document.getElementById("restart").innerHTML = "restart";
   document.body.style.backgroundImage = "url('./assets/bladerunner.png')";
 }
 function humans(){
@@ -36,10 +40,12 @@ function humans(){
 function flares(){
   document.getElementById("line2").innerHTML = "of solar flares burning bright";
   document.getElementById("line3").innerHTML = "tiny galaxies";
+  document.getElementById("restart").innerHTML = "restart";
   document.body.style.backgroundImage = "url('./assets/galaxies.png')";
 }
 function fires(){
   document.getElementById("line2").innerHTML = "of wildfires burning bright";
   document.getElementById("line3").innerHTML = "scorched from dragon's breath";
+  document.getElementById("restart").innerHTML = "restart";
   document.body.style.backgroundImage = "url('./assets/dragon.png')";
 }
