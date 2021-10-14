5fde0631f329fb855db1e52127f322f41174f222
cnakpil <85041294+cnakpil@users.noreply.github.com>
2021-10-14 12:33:18 -0600

    Update styling, js, index

--- week7/external_javascript.js
+++ week7/external_javascript.js
@@ -9,7 +9,14 @@ const quotes = ["Toddlers scream when distressed. Adults should too.",
 "bitch and bastard are GENDERED terms and thus you must use the neutral: bitchard",
 "*planet explodes* *removes one earbud* what",
 "don’t even pretend you didn’t learn the words famished and parched from nintendogs",
-"you’re only trying to ritually sacrifice me because you think i’m hot just admit it"];
+"you’re only trying to ritually sacrifice me because you think i’m hot just admit it",
+"Canon: Am I joke to you? <br> Fanon: No, just a disappointment.",
+"you ever have to shake your leg because there’s a rock in it?",
+"salad, or as i call it, expensive leaf nonsense",
+"If carp could speak they would all have French accents",
+"*pronounces “hors d'oeuvres” as “horse divorce”*",
+"if i have a daughter im going to name her lizard and then she’ll get the nickname liz and everyone will be like “oh is it short for elizabeth?” and she will have to say “no my name is lizard”",
+"kinda funny when english teachers say stuff like “i can tell if you didnt read the book” or “i can tell when people bs their paper” <br>no you cant.  you can tell when people are bad at bs-ing their paper.  i didnt even read the sparknotes and i barely skimmed the wikipedia and you gave me an A.  you kneel before my throne unaware that it was born of lies"];
 quoteCount = 0;
 document.getElementById('qbutton').addEventListener("click", newQuote);
 function newQuote(){
