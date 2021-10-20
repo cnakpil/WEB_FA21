3b55fc9b3f1617d3c302181d97ad665d73f0e6ef
cnakpil <85041294+cnakpil@users.noreply.github.com>
2021-10-20 17:02:14 -0600

    week8 javascript update
    added:
    - header change for day of week
    - date change for correct date
    - random generator for self care on refresh
    - correct reminders for the day of week

--- week8/external_javascript.js
+++ week8/external_javascript.js
@@ -1,1 +1,37 @@
-const selfCare = ["game", "write", "nap", "cook", "eat", "stretch", "read"];
+// generate random self care task on reload
+const selfCare = ["game", "write", "nap", "cook", "eat", "read"];
+const random = Math.floor(Math.random() * selfCare.length);
+document.getElementById("scItems").innerHTML = selfCare[random];
+
+//pull date data
+const mName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const dName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const d = new Date();
+let dayOfWeek = d.getDay();
+let day = d.getDate();
+let month = mName[d.getMonth()];
+let year = d.getFullYear();
+let currentDate = day+" "+month+" "+year;
+
+// Swap in current day of week for header
+document.getElementById("day").innerHTML = dName[dayOfWeek];
+// Swap in current date
+document.getElementById("date").innerHTML = currentDate;
+
+// reminder arrays
+const rSun = ["take walk","shower","meal prep"];
+const rMon = ["take walk","do dishes","clean up"];
+const rTues = ["trash run","shower","clean floors"];
+const rWed = ["take walk","do dishes","clean room"];
+const rThur = ["clean kitchen","shower","clean up"];
+const rFri = ["trash run","do dishes","clean floors"];
+const rSat = ["take walk","shower","laundry"];
+const wSun = [rSun, rMon, rTues, rWed, rThur, rFri, rSat];
+
+// set the array for current day
+let rToday = wSun[dayOfWeek];
+
+// set lines on page to correct lines for the day of week
+for(i=0; i<3; i++){
+  document.getElementById("check"+(i+1)).innerHTML = rToday[i];
+}
